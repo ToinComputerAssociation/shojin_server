878e1db1a4f6771a8c524329f3a09a24374928a0
strawberry29 <atcoder0929bot@gmail.com>
2023-11-08 12:08:13 +0000

    add newfile and some fixes

--- get.py
+++ get.py
@@ -2,13 +2,17 @@ from urllib import request
 import requests
 from bs4 import BeautifulSoup
 import os
-url_login = "https://atcoder.jp/login?lang=ja"
+
 session = requests.session()
+url_login = "https://atcoder.jp/login?lang=ja"
 # ログインページへのアクセス完了
 req_before_login = session.get(url_login)
 
 # ログインするための情報を準備する
-login_data = {"username": os.environ["USERNAME"], "password": os.environ["PASWORD"]}
+login_data = {
+    "username": os.environ["USERNAME"],
+    "password": os.environ["PASWORD"]
+}
 
 # ログインするためにcsrfトークンが必要となるため情報を取得
 bs = BeautifulSoup(req_before_login.text, "html.parser")
@@ -18,9 +22,9 @@ login_data["csrf_token"] = csrf_token
 # 2. ログインページで認証を行い、管理者ページへ遷移する
 req_after_login = session.post(url_login, data=login_data)
 # 3. 認証完了後のページで他ページへ遷移を行う
-url = "https://atcoder.jp/contests/arc106/submissions?f.Task=arc106_a&f.LanguageName=&f.Status=AC&f.User=blueberry1001"
+url = "https://atcoder.jp/contests/cf17-final/submissions?f.Task=cf17_final_a&f.LanguageName=&f.Status=AC&f.User=strawberry0929"
 req = session.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 
-ac_count = soup.find_all(attrs={ 'class': 'label label-success' })
-print(len(ac_count))
+first_ac_data_id = soup.find_all(attrs={"data-id": True})[-1]['data-id']
+print(first_ac_data_id)
--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-import requests
 import json
+import os
 import time
+from urllib import request
+import requests
+from bs4 import BeautifulSoup
+import asyncio
+from collections import deque
 
-lastgetSubmission = 1699206101
+session = requests.session()
+
+
+# atcoderにログイン
+def login_atcoder():
+    url_login = "https://atcoder.jp/login?lang=ja"
+    # ログインページへのアクセス完了
+    req_before_login = session.get(url_login)
+
+    # ログインするための情報を準備する
+    login_data = {
+        "username": os.environ["USERNAME"],
+        "password": os.environ["PASWORD"]
+    }
+
+    # ログインするためにcsrfトークンが必要となるため情報を取得
+    bs = BeautifulSoup(req_before_login.text, "html.parser")
+    csrf_token = bs.find(attrs={"name": "csrf_token"}).get("value")
+    login_data["csrf_token"] = csrf_token
+
+    # 2. ログインページで認証を行い、管理者ページへ遷移する
+    req_after_login = session.post(url_login, data=login_data)
+
+
+lastgetSubmission = 1699296101
+
+
 # APIを用いた提出データの取得
 def getSubmissionData(user_id):
     unix_second = lastgetSubmission
@@ -10,9 +41,6 @@ def getSubmissionData(user_id):
     response = requests.get(api_url)
     jsonData = response.json()
     return jsonData
-
-
-submissions = getSubmissionData("strawberry0929")
 
 
 # 各問題において最も新しいAC提出のみを取得する
@@ -25,7 +53,50 @@ def collectNewestAcceptedSubmissions(submissions):
     return submits
 
 
-newestSubmits = collectNewestAcceptedSubmissions(submissions)
+#初めてのACか判定
+async def isFirstAC(submission):
+    await asyncio.sleep(1)
+    url = f"https://atcoder.jp/contests/{submission['contest_id']}/submissions?f.Task={submission['problem_id']}&f.LanguageName=&f.Status=AC&f.User={submission['user_id']}"
+    req = session.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    first_ac_data_id = soup.find_all(attrs={"data-id": True})[-1]['data-id']#一番最初にACした提出のidを取得
+    return int(first_ac_data_id) == submission['id']
 
-for data in newestSubmits:
-    print(data)
+
+def getDifficulties():
+    api_url = "https://kenkoooo.com/atcoder/resources/problem-models.json"
+    response = requests.get(api_url)
+    jsonData = response.json()
+    return jsonData
+
+
+def getRating(username):
+    response = requests.get(f"https://atcoder.jp/users/{username}/history/json")
+    jsonData = response.json()
+    return jsonData[-1]["NewRating"]
+
+def point_calculation(submission, difficultiy):
+    rate = getRating(submission['user_id'])
+    rate = 0
+    basic_point = 1000
+    return basic_point * pow(2, (difficultiy - rate) / 400)
+	
+submissions = getSubmissionData("strawberry0929")
+newestSubmits = collectNewestAcceptedSubmissions(submissions)
+login_atcoder()
+difficulties = getDifficulties()
+firstAC_submissions = deque()
+for submission in newestSubmits.values():
+    isFirst = asyncio.run(isFirstAC(submission))
+    if isFirst:
+        firstAC_submissions.append(submission)
+while len(firstAC_submissions) != 0:
+    submission = firstAC_submissions.popleft()
+    difficultiy = difficulties[submission['problem_id']]['difficulty']
+    print(difficultiy, point_calculation(submission, difficultiy))
+os.system('git add .')
+os.system(f'git config --global user.email "{os.environ["EMAIL"]}"')
+os.system('git config --global user.name "strawberry29"')
+os.system('git commit -m "add newfile and some fixes"')
+os.system('git push origin main')
+
