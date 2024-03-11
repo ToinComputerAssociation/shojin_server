b4855393eb0e9464584980ae8b98b7268e919562
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2024-03-11 10:39:43 +0900

    add fixpoint command

--- cogs/develop.py
+++ cogs/develop.py
@@ -8,9 +8,19 @@ class Develop(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def recalc(self, ctx: commands.Context, user_id: str, submit_id: int, before_rate: int):
+    async def fixpoint(self, ctx: commands.Context, user_id: str, before_diff: int, after_diff: int):
         "レートが変更された際にこのコマンドを使って再計算できます"
-        await ctx.send("開発中")
+        cog = self.bot.cogs["Shojin"]
+        user_rate = cog.users[user_id]["rating"]
+        if before_diff < -10000:
+            before_score = 0
+        else:
+            before_score = cog.get_score(user_rate, before_diff)
+        before = cog.users[user_id]["score"]
+        cog.users[user_id]["score"] -= before_score
+        after_score = cog.get_score(user_rate, after_diff)
+        cog.users[user_id]["score"] += after_score
+        await ctx.send(f"{user_id}のスコアを更新しました。\n{before} -> {cog.users[user_id]['score']} (-{before_score:.3f} +{after_score:.3f})")
 
 
 async def setup(bot):
